081816a94b7d63d9d094abb7e7f90a83e3bf3d44
Nicolò Marconi <nicolo.marconi02@gmail.com>
2025-05-16 17:45:33 +0200

    fix: parallel multiplication

--- core/src/operations/gpu_matrix_vec.cuh
+++ core/src/operations/gpu_matrix_vec.cuh
@@ -11,15 +11,16 @@ __global__ void parallelMultiplication(indexType N, indexType* csr,
   if (startIndex > N) {
     return;
   }
-  indexType count = 0;
-  indexType beginRow = 0;
   indexType stride = blockDim.x * gridDim.x;
   for (indexType i = startIndex; i <= N; i += stride) {
-    count = csr[i - 1];
-    beginRow = count;
-    for (; count < beginRow + csr[i] - csr[i - 1]; count++) {
-      res[i - 1] += values[count] * vec[columns[count]];
+    indexType rowStart = csr[i - 1];
+    indexType rowEnd = csr[i];
+
+    dataType sum = 0;
+    for (indexType j = rowStart; j < rowEnd; j++) {
+      sum += values[j] * vec[columns[j]];
     }
+    res[i - 1] = sum;
   }
 }
 }  // namespace Operations
