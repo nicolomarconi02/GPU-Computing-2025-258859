f7fa0448d3d11daf0267fc90b0f565311d7a9ccf
Nicolò Marconi <nicolo.marconi02@gmail.com>
2025-05-18 00:26:21 +0200

    refactor: sequential multiplication cpu

--- core/src/operations/cpu_matrix_vec.hpp
+++ core/src/operations/cpu_matrix_vec.hpp
@@ -2,28 +2,27 @@
 
 #include "structures/matrix.hpp"
 #include "expected.hpp"
-#include "profiler/profiler.hpp"
 
 namespace Operations {
-enum MultiplicationTypes : uint8_t {
-  Sequential = 0,
-  SIZE
-};
+enum MultiplicationTypes : uint8_t { Sequential = 0, SIZE };
 template <typename indexType, typename dataType>
-tl::expected<Matrix<indexType, dataType>, std::string> sequentialMultiplication(const Matrix<indexType, dataType>& mat,
-                                             const Matrix<indexType, dataType>& vec) {
-  if(!(vec.type & MatrixType_::array)){
+tl::expected<Matrix<indexType, dataType>, std::string> sequentialMultiplication(
+    const Matrix<indexType, dataType>& mat,
+    const Matrix<indexType, dataType>& vec) {
+  if (!(vec.type & MatrixType_::array)) {
     return tl::make_unexpected("Cannot multiply. Vec must be an array!");
   }
   Matrix<indexType, dataType> res(MatrixType_::array, mat.N_ROWS);
-  int count = 0;
-  int beginRow = 0;
-  for (int i = 1; i <= mat.N_ROWS; i++) {
-    beginRow = count;
-    for (; count < beginRow + mat.csr[i] - mat.csr[i - 1]; count++) {
-      res.values[i - 1] += mat.values[count] * vec.values[mat.columns[count]];
+  for (indexType i = 1; i <= mat.N_ROWS; i++) {
+    indexType rowStart = mat.csr[i - 1];
+    indexType rowEnd = mat.csr[i];
+
+    dataType sum = 0;
+    for (indexType j = rowStart; j < rowEnd; j++) {
+      sum += mat.values[j] * vec.values[mat.columns[j]];
     }
+    res.values[i - 1] = sum;
   }
   return res;
 }
-}  // namespace operations
+}  // namespace Operations
